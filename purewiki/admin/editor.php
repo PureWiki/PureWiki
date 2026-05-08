ca544a54dfaf942a5ef689c31c99a40a4b4e3521
Oliver Weinhold <71061042+OliverWeinhold@users.noreply.github.com>
2026-05-08 22:44:02 +0200

    test: trigger CI with syntax error (fix)

--- purewiki/admin/editor.php
+++ purewiki/admin/editor.php
@@ -12,7 +12,7 @@
 
 defined('PUREWIKI') || die('Direct access denied.');
 
-require_once __DIR__ . '/../core/config.php'
+require_once __DIR__ . '/../core/config.php';
 require_once __DIR__ . '/../core/i18n.php';
 require_once __DIR__ . '/../core/asset_manager.php';
 require_once __DIR__ . '/../core/misc.php';
